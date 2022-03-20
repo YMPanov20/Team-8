59e2d0e9a988dcd0e00c47bec8d5cca10eeb1dd9
Димитър Василев <DSVasilev20@codingburgas.bg>
2022-03-20 16:41:54 +0200

    Add questions.js
    *Edit contact.html

---
+++ scripts/questions.js
@@ -0,0 +1,209 @@
+let questions = [
+    {
+    numb: 1,
+    question: "*",
+    answer: "*",
+    options: [
+      "*",
+      "*",
+      "*",
+      "*"
+    ]
+  },
+    {
+    numb: 2,
+    question: "*",
+    answer: "*",
+    options: [
+      "*",
+      "*",
+      "*",
+      "*"
+    ]
+  },
+    {
+    numb: 3,
+    question: "*",
+    answer: "* ",
+    options: [
+      "*",
+      "*",
+      "*",
+      "*"
+    ]
+  },
+    {
+    numb: 4,
+    question: "*",
+    answer: "*",
+    options: [
+      "*",
+      "*",
+      "*",
+      "*"
+    ]
+  },
+    {
+    numb: 5,
+    question: "*",
+    answer: "*",
+    options: [
+      "*",
+      "*",
+      "*",
+      "*"
+    ]
+  },
+  {
+    numb: 6,
+    question: "*",
+    answer: "*",
+    options: [
+      "*",
+      "*",
+      "*",
+      "*"
+    ]
+  },
+  {
+    numb: 7,
+    question: "*",
+    answer: "*",
+    options: [
+      "*",
+      "*",
+      "*",
+      "*"
+    ]
+  },
+  {
+    numb: 8,
+    question: "*",
+    answer: "*",
+    options: [
+      "*",
+      "*",
+      "*"
+    ]
+  },
+  {
+    numb: 9,
+    question: "*",
+    answer: "*",
+    options: [
+      "*",
+      "*",
+      "*"
+    ]
+  },
+  {
+    numb: 10,
+    question: "*",
+    answer: "*",
+    options: [
+      "*",
+      "*",
+      "*"
+    ]
+  },
+  {
+    numb: 11,
+    question: "*",
+    answer: "*",
+    options: [
+      "*",
+      "*",
+      "*"
+    ]
+  },
+  {
+    numb: 12,
+    question: "*",
+    answer: "*",
+    options: [
+      "*",
+      "*",
+      "*"
+    ]
+  },
+  {
+    numb: 13,
+    question: "*",
+    answer: "*",
+    options: [
+      "*",
+      "*",
+      "*"
+    ]
+  },
+  {
+    numb: 14,
+    question: "*",
+    answer: "*",
+    options: [
+      "*",
+      "*",
+      "*"
+    ]
+  },
+  {
+    numb: 15,
+    question: "*",
+    answer: "*",
+    options: [
+      "*",
+      "*",
+      "*"
+    ]
+  },
+  {
+    numb: 16,
+    question: "*",
+    answer: "*",
+    options: [
+      "*",
+      "*",
+      "*"
+    ]
+  },
+  {
+    numb: 17,
+    question: "*",
+    answer: "*",
+    options: [
+      "*",
+      "*",
+      "*"
+    ]
+  },
+  {
+    numb: 18,
+    question: "*",
+    answer: "*",
+    options: [
+      "*",
+      "*",
+      "*"
+    ]
+  },
+  {
+    numb: 19,
+    question: "*",
+    answer: "*",
+    options: [
+      "*",
+      "*",
+      "*"
+    ]
+  },
+  {
+    numb: 20,
+    question: "*",
+    answer: "*",
+    options: [
+      "*",
+      "*",
+      "*"
+    ]
+  },
+];
