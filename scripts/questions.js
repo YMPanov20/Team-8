ca0aebb77c08a30dcfbfd0733b561fa550919e4b
Димитър Василев <DSVasilev20@codingburgas.bg>
2022-03-20 19:44:07 +0200

    Update questions.js

--- scripts/questions.js
+++ scripts/questions.js
@@ -1,209 +1,222 @@
 let questions = [
-    {
-    numb: 1,
-    question: "*",
-    answer: "*",
-    options: [
-      "*",
-      "*",
-      "*",
-      "*"
-    ]
-  },
-    {
-    numb: 2,
-    question: "*",
-    answer: "*",
-    options: [
-      "*",
-      "*",
-      "*",
-      "*"
-    ]
-  },
-    {
-    numb: 3,
-    question: "*",
-    answer: "* ",
-    options: [
-      "*",
-      "*",
-      "*",
-      "*"
-    ]
-  },
-    {
-    numb: 4,
-    question: "*",
-    answer: "*",
-    options: [
-      "*",
-      "*",
-      "*",
-      "*"
-    ]
-  },
-    {
-    numb: 5,
-    question: "*",
-    answer: "*",
-    options: [
-      "*",
-      "*",
-      "*",
-      "*"
-    ]
-  },
   {
-    numb: 6,
-    question: "*",
-    answer: "*",
-    options: [
-      "*",
-      "*",
-      "*",
-      "*"
-    ]
-  },
+  numb: 1,
+  question: "Which is the smallest city in Bulgaria?",
+  answer: "Obzor",
+  options: [
+    "Hisarya",
+    "Obzor",
+    "Belogradchik",
+    "Tsarevo"
+  ]
+},
   {
-    numb: 7,
-    question: "*",
-    answer: "*",
-    options: [
-      "*",
-      "*",
-      "*",
-      "*"
-    ]
-  },
+  numb: 2,
+  question: "When The Battle of Achelous took place?",
+  answer: "August 917",
+  options: [
+    "August 918",
+    "September 919",
+    "August 917",
+    "July 917"
+  ]
+},
   {
-    numb: 8,
-    question: "*",
-    answer: "*",
-    options: [
-      "*",
-      "*",
-      "*"
-    ]
-  },
+  numb: 3,
+  question: "Which alphabet ivented Saints Cyril and Methodius?",
+  answer: "Glagolitic ",
+  options: [
+    "Latin",
+    "Cyrillic",
+    "Glagolitic ",
+    "Arabic"
+  ]
+},
   {
-    numb: 9,
-    question: "*",
-    answer: "*",
-    options: [
-      "*",
-      "*",
-      "*"
-    ]
-  },
+  numb: 4,
+  question: "From which people Bulgaria has derived its name?",
+  answer: "Bulgars",
+  options: [
+    "Bulgars",
+    "Goths",
+    "Huns",
+    "Vandals"
+  ]
+},
   {
-    numb: 10,
-    question: "*",
-    answer: "*",
-    options: [
-      "*",
-      "*",
-      "*"
-    ]
-  },
-  {
-    numb: 11,
-    question: "*",
-    answer: "*",
-    options: [
-      "*",
-      "*",
-      "*"
-    ]
-  },
-  {
-    numb: 12,
-    question: "*",
-    answer: "*",
-    options: [
-      "*",
-      "*",
-      "*"
-    ]
-  },
-  {
-    numb: 13,
-    question: "*",
-    answer: "*",
-    options: [
-      "*",
-      "*",
-      "*"
-    ]
-  },
-  {
-    numb: 14,
-    question: "*",
-    answer: "*",
-    options: [
-      "*",
-      "*",
-      "*"
-    ]
-  },
-  {
-    numb: 15,
-    question: "*",
-    answer: "*",
-    options: [
-      "*",
-      "*",
-      "*"
-    ]
-  },
-  {
-    numb: 16,
-    question: "*",
-    answer: "*",
-    options: [
-      "*",
-      "*",
-      "*"
-    ]
-  },
-  {
-    numb: 17,
-    question: "*",
-    answer: "*",
-    options: [
-      "*",
-      "*",
-      "*"
-    ]
-  },
-  {
-    numb: 18,
-    question: "*",
-    answer: "*",
-    options: [
-      "*",
-      "*",
-      "*"
-    ]
-  },
-  {
-    numb: 19,
-    question: "*",
-    answer: "*",
-    options: [
-      "*",
-      "*",
-      "*"
-    ]
-  },
-  {
-    numb: 20,
-    question: "*",
-    answer: "*",
-    options: [
-      "*",
-      "*",
-      "*"
-    ]
-  },
+  numb: 5,
+  question: "From what year Bulgaria is democratic?",
+  answer: "1989",
+  options: [
+    "1999",
+    "1988",
+    "1998",
+    "1989"
+  ]
+},
+{
+  numb: 6,
+  question: "When was the serbo-bulgarian war?",
+  answer: "1885",
+  options: [
+    "1885",
+    "1886",
+    "1888",
+    "1900"
+  ]
+},
+{
+  numb: 7,
+  question: "Which is the oldest city in Bulgaria?",
+  answer: "Plovdiv",
+  options: [
+    "Burgas",
+    "Plovdiv",
+    "Sofia",
+    "Pliska"
+  ]
+},
+{
+  numb: 8,
+  question: "What is the name of the only desert in Bulgaria?",
+  answer: "Pobiti Kamani",
+  options: [
+    "There is no desert in Bulgaria.",
+    "Pobiti Kamani",
+    "Kamen Bryag",
+    "Zlatni Pyasaci"
+  ]
+},
+{
+  numb: 9,
+  question: "The longest river in Bulgaria is",
+  answer: "Iskar",
+  options: [
+    "Iskar",
+    "Dunav",
+    "Maritsa",
+    "Arda"
+  ]
+},
+{
+  numb: 10,
+  question: "In what year was the first Bulgarian state established?",
+  answer: "681",
+  options: [
+    "688",
+    "682",
+    "681",
+    "680"
+  ]
+},
+{
+  numb: 11,
+  question: "When was The Treaty of Berlin signed?",
+  answer: "July 13, 1878",
+  options: [
+    "June 13, 1878",
+    "July 13, 1879",
+    "July 13, 1878",
+    "June 13, 1879"
+  ]
+},
+{
+  numb: 12,
+  question: "Under which King Bulgarians became Christians?",
+  answer: "Boris I",
+  options: [
+    "Cyril",
+    "Basil II",
+    "Boris I",
+    "Samuil"
+  ]
+},
+{
+  numb: 13,
+  question: "The longest mountain in Bulgaria is?",
+  answer: "Stara planina",
+  options: [
+    "Rila",
+    "Stara planina",
+    "Rodopi",
+    "Vitosha"
+  ]
+},
+{
+  numb: 14,
+  question: "The Mount Shulep Kamak is the place were Bulgaria limit with",
+  answer: "Greece and Turkey",
+  options: [
+    "Macedonia and Serbia",
+    "Serbia and Romania",
+    "Greece and Turkey",
+    "Macedonia and Greece"
+  ]
+},
+{
+  numb: 15,
+  question: "How many kilometres of coastline does Bulgaria have?",
+  answer: "354km",
+  options: [
+    "435km",
+    "167km",
+    "354km",
+    "390km"
+  ]
+},
+{
+  numb: 16,
+  question: "Who was the first bulgarian president?",
+  answer: "Zhelyu Zhelev",
+  options: [
+    "Petar Stoyanov",
+    "Rumen Radev",
+    "Zhelyu Zhelev",
+    "Rosen Plevneliev"
+  ]
+},
+{
+  numb: 17,
+  question: "What percentage of Bulgarians live in towns or cities?",
+  answer: "71%",
+  options: [
+    "98%",
+    "82%",
+    "79%",
+    "71%"
+  ]
+},
+{
+  numb: 18,
+  question: "The official religion in Bulgaria is",
+  answer: "Orthodox christianity",
+  options: [
+    "Orthodox christianity",
+    "Catholic christianity",
+    "Islam",
+    "Judaism"
+  ]
+},
+{
+  numb: 19,
+  question: "Which is the biggest country bordering Bulgaria?",
+  answer: "Turkey",
+  options: [
+    "Romania",
+    "Greece",
+    "Serbia",
+    "Turkey"
+  ]
+},
+{
+  numb: 20,
+  question: "Which is the Bulgarian most famous seaside resort?",
+  answer: "Sunny Beach",
+  options: [
+    "Kiten",
+    "Tsarevo",
+    "Sunny Beach",
+    "Primosrsko"
+  ]
+},
 ];
